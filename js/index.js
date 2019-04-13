781123c6910dc82c2ddee5b522f3b037f53a4ff8
Wes-White <wesleyrwhite86@gmail.com>
2019-04-13 10:43:13 -0700

    changed some features and added new event, 1 more to go

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const head = document.querySelector('header')
 const foot = document.querySelector('footer')
 const clickers = document.querySelectorAll('.btn')
 const banner = document.querySelector('.bannerBus')
-const pixs = document.querySelectorAll('img')
+const pixs = document.querySelectorAll('.img')
 const anchors = document.querySelector('.nav, a')
 
 
@@ -50,7 +50,7 @@ banner.addEventListener('dblclick', (event)=> {
 })
 
 4. // Change Banner to boom-bus
-banner.addEventListener('mouseover', (event) => { 
+banner.addEventListener('contextmenu', (event) => { 
     banner.setAttribute('src', 'img/boom-bus.jpg')
 })
    
@@ -63,12 +63,27 @@ pixs.forEach(image => {
 })
 
 6. // Nav Anchors Talk With a Click 
-anchors.addEventListener('click', (object) => {
+anchors.addEventListener('click', (event) => {
  alert("Ouch!! why did you poke me?")
 })
 
 7. //Log to Console when Window is Resized. 
-window.addEventListener('resize', (object) =>{
+window.addEventListener('resize', (event) =>{
     console.log('I hope this is Responsive....')
 })
-    
+
+8. // Hide Text 
+allPage.addEventListener('keyup', function(event){ 
+    // let backDrop = 
+    if(event.key == 't'){ 
+        allPage.style.color = 'white'
+        alert("Where's the text?")
+    }
+})
+
+9. // Alert User on Load 
+window.addEventListener('load', function (event){ 
+    alert("check out www.thebussauce.com for secret featurs of this site.")
+})
+
+10. // 
