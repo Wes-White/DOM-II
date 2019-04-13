03609a033e70457a70cf01036dfe63fe41c7fdf0
Wes-White <wesleyrwhite86@gmail.com>
2019-04-13 10:03:47 -0700

    added dark mode

--- js/index.js
+++ js/index.js
@@ -2,3 +2,36 @@
 
 
 //Variables 
+
+const allPage = document.querySelector('html')
+const head = document.querySelector('header')
+const foot = document.querySelector('footer')
+const clickers = document.querySelectorAll('.btn')
+const banner = document.querySelector('.bannerBus')
+
+
+
+
+
+//Event Listeners 
+
+1. //Dark Mode 
+allPage.addEventListener('keydown', function(event){
+    if(event.key == 'd'){ 
+    allPage.style.backgroundColor = 'black'
+    foot.style.backgroundColor = 'black'
+    head.style.backgroundColor = 'black'
+    allPage.style.color = 'white'
+    alert('Press w for Default Page')
+    }   
+    
+    if(event.key == 'w'){ 
+        allPage.style.backgroundColor = 'white'
+        foot.style.backgroundColor = 'white'
+        head.style.backgroundColor = 'white'
+        allPage.style.color = 'black'
+        alert('Press d for DarkMode')
+    }
+
+})
+
